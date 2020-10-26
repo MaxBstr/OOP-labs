cd51fc8258b091159573bbc51ea7bf516acd3f2d
max_bstr <60942444+MaxBstr@users.noreply.github.com>
2020-10-26 12:19:26 +0300

    Update Shop.cs

--- lab2/Shop.cs
+++ lab2/Shop.cs
@@ -1,52 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Shop
 {
-    public class Item
-    {
-        private string _itemName;
-        private int _itemAmount, _unitPrice;
-        private int _itemID = 0;
-
-        public int ItemID
-        {
-            get => _itemID; 
-            set => _itemID = value;
-        }
-        public string ItemName => _itemName;
-
-        public int UnitPrice
-        {
-            get => _unitPrice;
-            set => _unitPrice = value;
-        }
-
-        public int ItemAmount
-        {
-            get => _itemAmount;
-            set => _itemAmount = value;
-        }
-
-        public Item(string itemName, int itemAmount, int unitPrice)
-        {
-            this._itemName = itemName;
-            this._itemAmount = itemAmount;
-            this._unitPrice = unitPrice;
-        }
-    }
-
     public class Shop
     {
-        private int _shopID;
-        private static int _shopsCount = 0;
-        private string _shopName, _shopAdress;
-        public string ShopName => _shopName; 
-        public string ShopAdress => _shopAdress;
-        public int ShopID => _shopID;
-        
-        private List<Item> ShopItems = new List<Item>();
-        public List<Item> ShopItemsGet => ShopItems;
+        private static int _shopsCount;
+        public string ShopName { get; }
+
+        public string ShopAddress { get; }
+
+        public int ShopId { get; }
+
+        public List<Item> ShopItemsGet { get; } = new List<Item>();
+
         public Shop(string shopName, string shopAddress)
         {
             if (shopName == "" || shopAddress == "")
@@ -55,19 +22,19 @@ namespace Shop
                 return;
             }
             _shopsCount++;
-            _shopID = _shopsCount;
-            this._shopName = shopName ?? throw new ArgumentNullException(nameof(shopName));
-            this._shopAdress = shopAddress ?? throw new ArgumentNullException(nameof(shopAddress));
+            ShopId = _shopsCount;
+            this.ShopName = shopName;
+            this.ShopAddress = shopAddress;
         }
 
         public void AddProduct(Item newItem)
         {
-            var existItem = ShopItems.Find(x => x.ItemName == newItem.ItemName && x.UnitPrice == newItem.UnitPrice);
+            var existItem = ShopItemsGet.Find(x => x.ItemName == newItem.ItemName && x.UnitPrice == newItem.UnitPrice);
             if (existItem != null) existItem.ItemAmount += newItem.ItemAmount;
             else
             {
-                ShopItems.Add(newItem);
-                newItem.ItemID = ShopItems.Count;
+                ShopItemsGet.Add(newItem);
+                newItem.ItemId = ShopItemsGet.Count;
             }
         }
 
@@ -75,12 +42,12 @@ namespace Shop
         {
             foreach (var newItem in newItems)
             {
-                var existItem = ShopItems.Find(x => x.ItemName == newItem.ItemName && x.UnitPrice == newItem.UnitPrice);
+                var existItem = ShopItemsGet.Find(x => x.ItemName == newItem.ItemName && x.UnitPrice == newItem.UnitPrice);
                 if (existItem != null) existItem.ItemAmount += newItem.ItemAmount;
                 else
                 {
-                    ShopItems.Add(newItem);
-                    newItem.ItemID = ShopItems.Count;
+                    ShopItemsGet.Add(newItem);
+                    newItem.ItemId = ShopItemsGet.Count;
                 }
             }
         }
@@ -89,14 +56,6 @@ namespace Shop
         {
             newItem.UnitPrice = unitPrice;
         }
-
-        public void PrintCart()
-        {
-            foreach (var item in ShopItems)
-            {
-                Console.WriteLine($"ID: {item.ItemID}\nItem: {item.ItemName}\nAmount: {item.ItemAmount}\nUnitPrice: {item.UnitPrice}");
-                Console.WriteLine("========================");
-            }
-        }
+        
     }
 }
