967d9c860a714b747586b0c4ae8efbfad058531f
max_bstr <60942444+MaxBstr@users.noreply.github.com>
2020-11-09 12:05:33 +0300

    Add files via upload

---
+++ lab3/Race.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Races
+{
+    internal class Race <T> where T : Transport
+    {
+        //air, ground, all
+        private readonly double _raceDistance;
+        private readonly List<T> _vehicles = new List<T>();
+
+        public Race(double raceDistance)
+        {
+            this._raceDistance = raceDistance;
+        }
+
+        public void RegisterVehicle(params T[] vehicles)
+        {
+            if (vehicles.Length == 0)
+                throw new Exception("No vehicles were found!");
+            
+            foreach (var veh in vehicles)
+            {
+                _vehicles.Add(veh);
+            }
+        }
+
+        public Transport StartRace()
+        {
+            if (_vehicles.Count == 0)
+                throw new Exception("You can`t start race without racers!");
+            
+            var resultTime = double.MaxValue;
+            Transport winner = null;
+
+            foreach (var vehicle in _vehicles)
+            {
+                var time = vehicle.CalcTime(_raceDistance);
+                if (time >= resultTime) continue;
+                
+                resultTime = time;
+                winner = vehicle;
+            }
+
+            return winner;
+        }
+
+    }
+}
---
+++ lab3/Transport.cs
@@ -0,0 +1,20 @@
+﻿namespace Races
+{
+    internal abstract class Transport
+    {
+        protected abstract double Speed { get; }
+        public abstract double CalcTime(double distance);
+
+    }
+
+    internal abstract class GroundTransport : Transport
+    {
+        protected abstract double RestInterval { get; }
+        protected abstract double RestDuration { get; set; }
+    }
+    internal abstract class AirTransport : Transport
+    {
+        protected abstract double DistanceReducer { get; }
+    }
+    
+}
